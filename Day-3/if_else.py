969a1396098b6a3c2a6076596f86fe8cec7c72f3
GulshanPrajapati <60136202+GulshanPrajapati@users.noreply.github.com>
2020-09-13 11:53:26 +0530

    Day-3

---
+++ Day-3/if_else.py
@@ -0,0 +1,7 @@
+num =int(input("Enter the height to land : "))
+if num < 1000:
+    print("Safe to land")
+elif num < 4500:
+    print("bring donw to 1000")
+elif num < 6500:
+    print("Turn Around")
