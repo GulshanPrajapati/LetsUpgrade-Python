a4172ae4e630b05ef8b5e4eef924992c935b1171
GulshanPrajapati <60136202+GulshanPrajapati@users.noreply.github.com>
2020-09-13 11:55:19 +0530

    Update prime.py

--- Day-3/prime.py
+++ Day-3/prime.py
@@ -1,7 +1,5 @@
-lower = int(input("Enter lower range: "))  
-upper = int(input("Enter upper range: "))  
-  
-for num in range(lower,upper + 1):  
+
+for num in range(1,200):  
    if num > 1:  
        for i in range(2,num):  
            if (num % i) == 0:  
